552652a817c9673e7ca4a414b2b6c9a9e4559b08
jarble <jarble@gmail.com>
2016-09-03 15:15:44 -0400

    updated file name

--- pack.pl
+++ pack.pl
@@ -2,11 +2,11 @@ name('transpiler').
 title('A universal translator for programming languages').
 
 version('0.1').
-download('https://github.com/jarble/universal-transpiler/archive/v0.1.zip').
+download('https://github.com/jarble/transpiler/archive/v0.1.zip').
 
 
 author('Anderson Green','jarble@gmail.com').
 packager('Anderson Green','jarble@gmail.com').
 maintainer('Anderson Green','jarble@gmail.com').
 
-home('https://github.com/jarble/universal-transpiler/').
+home('https://github.com/jarble/transpiler/').
