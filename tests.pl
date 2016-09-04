f4a932a9dab2e4a35124f04d2b687902ed9e8ba0
jarble <jarble@gmail.com>
2016-09-04 00:11:50 -0400

    Updated tests

--- tests.pl
+++ tests.pl
@@ -5,8 +5,4 @@
 
 main :-
 	File='input.txt',read_file_to_codes(File,Input_,[]),atom_codes(Input,Input_),
-	writeln(Input),translate_langs(Input),
-	
-	writeln('\n\nTranslated from PHP and JavaScript:\n\n'),
-	writeln(translate $('function add($a,$b){return $a + $b;}',prolog)),
-	writeln(translate $('class example{constructor(a,b){this.a = a + 1; this.b = b + 1;}}','perl')).
+	writeln(Input),translate_langs(Input).
