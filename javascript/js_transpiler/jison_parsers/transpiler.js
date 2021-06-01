8a0e3c8b32b80208bae52e73d0e5d66f8103343a
jarble <jarble@gmail.com>
2021-06-01 11:51:46 -0500

    converted to a module

--- javascript/js_transpiler/jison_parsers/transpiler.js
+++ javascript/js_transpiler/jison_parsers/transpiler.js
@@ -1,4 +1,6 @@
 "use strict";
+import ternary_operator from '../modules/code_generator.mjs';
+
 var types = {};
 var defined_vars = [];
 var global_vars = [];
@@ -12636,43 +12638,7 @@ function is_metasyntax(lang){
 	return member(lang,["nearley","ometa","ohm","pyparsing","lemon","chrg","instaparse","parboiled","javacc","waxeye","rebol","lpeg","txl","treetop","abnf","peg.js","antlr","marpa","wirth syntax notation","jison"])
 }
 
-function ternary_operator(lang,a,b,c){
-		return member(lang,["java","openscad","reasonml","vala","sidef","dart","tcl","autohotkey","javascript","typescript","c","c#","haxe","c++","perl","ruby","julia","awk","swift"])
-			? a+"?"+b+":"+c
-		: member(lang,["php","hack"])
-			? "("+a+"?"+b+":"+c+")"
-		: member(lang,["perl 6"])
-			? "("+a+"??"+b+"!!"+c+")"
-		: member(lang,["algol 68"])
-			? "("+a+"|"+b+"|"+c+")"
-		: member(lang,["modelica","ocaml","lustre","isabelle/hol","xquery","futhark","coffeescript","standard ml","ada","coq","chapel","haskell","ada","mercury"])
-			? "(if "+a+" then "+b+" else "+c+")"
-		: member(lang,["python","coconut"])
-			? "("+b+" if "+a+" else "+c+")"
-		: member(lang,["fermat"])
-			? " if"+a+" then "+b+" else "+c+"fi "
-		: member(lang,["nim"])
-			? "(if "+a+": " +b+ " else: "+c+")"
-		: member(lang,["rust"])
-			? "(if "+a+"{"+b+"} else {"+c+"})"
-		: member(lang,["lua"])
-			? "(("+a+") and ("+b+") or ("+c+"))"
-		: member(lang,["visual basic .net"])
-			? "If("+a+","+b+","+c+")"
-		: member(lang,["octave"])
-			? "ifelse("+a+","+b+","+c+")"
-		: member(lang,["smt-lib"])
-			? "(ite "+a+" "+b+" "+c+")"
-		: member(lang,["minizinc"])
-			? "if "+a+" then "+b+" else "+c+" endif"
-		: member(lang,["alloy"])
-			? "(("+a+") implies ("+b+") else ("+c+"))"
-		: member(lang,["mediawiki"])
-			? "{{#ifexpr:"+a+"|"+b+"|"+c+"}}"
-		: member(lang,["coffeescript","kotlin","r","scala"])
-			? "(if ("+a+") "+b+" else "+c+")"
-		: undefined;
-}
+
 
 function types_list_(input_lang,lang,a){
 	return a.map(function(x) {
---
+++ javascript/js_transpiler/peggy_parsers/code_generators.js
@@ -0,0 +1,9 @@
+//import this file into each peggy parser
+
+function member(lang,list){
+	return list.indexOf(lang) !== -1;
+}
+
+export function concatenate_string(lang){
+	
+}
