8b7187bcdfa5b9207466c0325d8a3f727d53b02e
jarble <jarble@gmail.com>
2021-06-26 18:37:22 -0500

    updated service worker

--- sw.js
+++ sw.js
@@ -79,14 +79,14 @@ var URLS = [                            // Add URL you want to cache in this lis
 
 // Respond with cached resources
 self.addEventListener('fetch', function (e) {
-  console.log('fetch request : ' + e.request.url)
+  //console.log('fetch request : ' + e.request.url)
   e.respondWith(
     caches.match(e.request).then(function (request) {
       if (request) { // if cache is available, respond with cache
-        console.log('responding with cache : ' + e.request.url)
+        //console.log('responding with cache : ' + e.request.url)
         return request
       } else {       // if there are no cache, try fetching request
-        console.log('file is not cached, fetching : ' + e.request.url)
+        //console.log('file is not cached, fetching : ' + e.request.url)
         return fetch(e.request)
       }
 
@@ -100,7 +100,7 @@ self.addEventListener('fetch', function (e) {
 self.addEventListener('install', function (e) {
   e.waitUntil(
     caches.open(CACHE_NAME).then(function (cache) {
-      console.log('installing cache : ' + CACHE_NAME)
+      //console.log('installing cache : ' + CACHE_NAME)
       return cache.addAll(URLS)
     })
   )
@@ -120,7 +120,7 @@ self.addEventListener('activate', function (e) {
 
       return Promise.all(keyList.map(function (key, i) {
         if (cacheWhitelist.indexOf(key) === -1) {
-          console.log('deleting cache : ' + keyList[i] )
+          //console.log('deleting cache : ' + keyList[i] )
           return caches.delete(keyList[i])
         }
       }))
