a97dc8724c5e97eb249a593dfca0315e131c9216
jarble <jarble@gmail.com>
2021-06-26 16:32:53 -0500

    added cache URLs to serviceworker

--- sw.js
+++ sw.js
@@ -2,8 +2,8 @@ var APP_PREFIX = 'ApplicationName_'     // Identifier for this app (this needs t
 var VERSION = 'version_01'              // Version of the off-line cache (change this value everytime you want to update cache)
 var CACHE_NAME = APP_PREFIX + VERSION
 var URLS = [                            // Add URL you want to cache in this list.
-  '/{repository}/',                     // If you have separate JS/CSS files,
-  '/{repository}/index.html'            // add path to those files here
+  'https://jarble.github.io/transpiler/',                     // If you have separate JS/CSS files,
+  'https://jarble.github.io/transpiler/javascript/js_transpiler/test_parser.html'            // add path to those files here
 ]
 
 // Respond with cached resources
