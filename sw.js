2fca0ecc93219bbaf21872c91ed13457ea96ac81
jarble <jarble@gmail.com>
2021-06-27 10:03:21 -0500

    update serviceworker

--- sw.js
+++ sw.js
@@ -2,7 +2,6 @@ var APP_PREFIX = 'ApplicationName_'     // Identifier for this app (this needs t
 var VERSION = 'version_01'              // Version of the off-line cache (change this value everytime you want to update cache)
 var CACHE_NAME = APP_PREFIX + VERSION
 var URLS = [                            // Add URL you want to cache in this list.
-'https://jarble.github.io/favicon.ico',
 'https://jarble.github.io/transpiler/javascript/js_transpiler/jison_parsers/java_parser.js',
 'https://jarble.github.io/transpiler/javascript/js_transpiler/jison_parsers/alloy_parser.js',
 'https://jarble.github.io/transpiler/javascript/js_transpiler/jison_parsers/tex_parser.js',
