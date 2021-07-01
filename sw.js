0eefcc75cc46511bcdd7b722f3dd51a732744d5f
jarble <jarble@gmail.com>
2021-07-01 15:18:01 -0500

    added another parser (still a work-in-progress)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 var APP_PREFIX = 'ApplicationName_'     // Identifier for this app (this needs to be consistent across every cache update)
-var VERSION = 'version_02'              // Version of the off-line cache (change this value everytime you want to update cache)
+var VERSION = 'version_04'              // Version of the off-line cache (change this value everytime you want to update cache)
 var CACHE_NAME = APP_PREFIX + VERSION
 var URLS = [                            // Add URL you want to cache in this list.
 'https://jarble.github.io/transpiler/javascript/js_transpiler/jison_parsers/java_parser.js',
